5c38b775866696d667d27bd45e9ea88b22054085
newazbenalam <newazbenalam@gmail.com>
2023-11-30 23:56:52 +0600

    flask secrets

--- app/__init__.py
+++ app/__init__.py
@@ -2,11 +2,14 @@ from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "flask" # FLASK_SECRET
+
+# FLASK_SECRET
+app.config['SECRET_KEY'] = "flask" 
 
 # MySQL configurations
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://mysql:12345@localhost:3306/testdb' 
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
+# Import Routes
 from app import routes
